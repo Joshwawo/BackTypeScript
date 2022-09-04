b7f47158d91f1004291101d94686edd65c8a7516
Joshwawo <masacota_masacota@hotmail.com>
2022-09-03 17:36:31 -0700

    add:mongo conn

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,22 @@
-import express from 'express'
-import cors from 'cors'
-import 'dotenv/config'
-import {router} from './routes'
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import { router } from "./routes";
+import db from "./config/mongo";
 
 const PORT = process.env.PORT || 3000;
 
+const app = express();
+app.use(cors());
+app.use(router);
+db()
+  .then(() => {
+    console.log("Con exitosa ");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
-const app = express()
-app.use(cors())
-app.use(router)
-
-app.listen(PORT, ()=>{
-    console.log(`Servidor corriendo en el http://localhost:${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el http://localhost:${PORT}`);
+});
