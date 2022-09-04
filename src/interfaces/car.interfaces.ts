e9f7506a2030a64a27deeb82cff5591ccc6339c1
Joshwawo <masacota_masacota@hotmail.com>
2022-09-03 18:16:59 -0700

    add:Models w/types

--- src/interfaces/car.interfaces.ts
+++ src/interfaces/car.interfaces.ts
@@ -1,7 +1,7 @@
 export interface Car {
     color:string,
     gas: "gasoline" | "electric"
-    year: string,
+    year: number,
     description: string
     price: number
 }
---
+++ src/models/items.ts
@@ -0,0 +1,31 @@
+import { Schema, Types, model, Model } from "mongoose";
+import { Car } from "../interfaces/car.interfaces";
+
+const ItemSchema = new Schema<Car>(
+  {
+    color: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+    gas: {
+      type: String,
+      enum: ["gasoline", "electric"],
+    },
+    price: {
+      type: Number,
+    },
+    year: {
+      type: Number,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+const ItemModel = model("items", ItemSchema);
+
+export default ItemModel;
